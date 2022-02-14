d62953f5a1fb6d07d018e518858a033ee9914ca8
Marco-Garabello <marco.garabello04@gmail.com>
2022-02-14 17:23:57 +0100

    progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
-import { readable } from "svelte/store";
 import { read_doc } from "./db.js";
 import { onMount } from 'svelte';
+import { each } from "svelte/internal";
 
 let item;
 let url;
+let container;
 onMount(async () => {
     item = await read_doc("SLC", "link1");
     url = item.url;
     console.log(item.url);
+
     
 });
+
 
 </script>
 
@@ -20,11 +23,12 @@ onMount(async () => {
   <div>
     <input type="text" name="searchBar" id="idSearchBar" placeholder="Search" />
   </div>
-  <div>
-    <textarea name="ciao" id="" cols="30" rows="10">
-    {item}
-    </textarea>
-  </div>
+  
+  <!-- {#each  as }
+      
+  {/each }
+       -->
+  
 </main>
 
 <style>
